dba2f45a83368ccaa0434ac69ef4778aa5566786
Namrita365 <22kt1a0576@gmail.com>
2025-06-23 00:26:21 +0530

    updated first with differnt types to display outputs

--- first.java
+++ first.java
@@ -1,5 +1,11 @@
 public class first {
     public static void main(String args[]){
-        System.out.print("Hello World");
+        System.out.print("Hello world!!!\n");
+        System.out.printf("Hiii Namrita");
+        System.out.println("\nHow do you do ?");
+        System.out.println("all the best");
+        
+
     }
 }
+
