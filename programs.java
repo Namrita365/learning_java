a22db9523808d8d50587857f32bc573c3e3b1d67
Namrita365 <22kt1a0576@gmail.com>
2025-06-23 00:42:56 +0530

    variables and datatypes

---
+++ programs.java
@@ -0,0 +1,24 @@
+public class programs {
+    public static void main(String args[]){
+        //variables and data types
+        String sname="Namrita";
+        System.out.println("Hey "+sname);
+        int age=21;
+        System.out.println("you are"+age+"year old");
+        String branch="CSE";
+        System.out.println("you took "+branch);
+        float cgpa=8.80f;
+        short rank=1;
+        char gender='F';
+        double per=85.5;
+        long phn_num=9834567832L;
+        boolean vote=true;
+        System.out.println("yayy!! you got "+rank+"st rank");
+        System.out.println("you got "+cgpa+"cgpa");
+        System.out.println("percntage is "+per);
+        System.out.println("you are women-"+gender);
+        System.out.println("your mob no is"+phn_num);
+        System.out.println(vote+", you are  eligible for voting ");
+    }
+    
+}
