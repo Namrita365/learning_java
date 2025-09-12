162c52ed4a99ac640717ab97975fb31fb17fe493
Namrita365 <22kt1a0576@gmail.com>
2025-09-13 00:53:33 +0530

    modified the code

--- java_training/day-4/loops/sum_natural_num.java
+++ java_training/day-4/loops/sum_natural_num.java
@@ -2,10 +2,19 @@ public class sum_natural_num {
         public static void main(String args[])
     {
         int i,sum=0,n=10;
+        System.out.print("sum of natural numbers:");
         for(i=1;i<=n;i++){
             sum=sum+i;
+            if(i<n){
+                System.out.print(i+"+");
+            }
+            else{
+
+                System.out.print(i);
+            }
+            
         }
-          System.out.println(sum);
+        System.out.println("="+sum);
 
     }
 
