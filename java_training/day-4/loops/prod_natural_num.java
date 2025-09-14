a75ae2749d93a11c6dbc8876d8cf7a9942133b9c
Namrita365 <22kt1a0576@gmail.com>
2025-09-14 16:39:07 +0530

    modify the code

--- java_training/day-4/loops/prod_natural_num.java
+++ java_training/day-4/loops/prod_natural_num.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 public class prod_natural_num {
     public static void main(String args[]){
-        int i,prod=1,n=10;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n=sc.nextInt();
+        int i,prod=1;
         for(i=1;i<=n;i++){
-            prod=prod*1;
+            prod=prod*i;
         }
         System.out.println(prod);
+        sc.close();
     }
 }
