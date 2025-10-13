62ec64c151e8282584ba311a407488b5f42bc9b7
Namrita365 <22kt1a0576@gmail.com>
2025-10-14 00:15:06 +0530

    Creating an object and modifying its instance variables.

--- self_learn_java_from_yt/oops/classObj.java
+++ self_learn_java_from_yt/oops/classObj.java
@@ -14,12 +14,17 @@ bag(String col,int p){
 public class classObj{
 
     public static void main(String args[]){
+        
         bag b1=new bag("black",2000);
+        b1.color="yellow";
+        b1.price=1000;
+
+
         bag b2=new bag("blue",3000);
         System.out.println("the colour of bag is "+b1.color+" and it is "+b1.price+" rupeees");
         b1.display();
         System.out.println("The bag in the "+b2.color+" is mine and it is of "+b2.price+" rupees");
         b2.display();
-        
+
     }
 }
