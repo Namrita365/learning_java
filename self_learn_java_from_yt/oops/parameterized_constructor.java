d3a13a1a4c27abb299456fe9179c7851aa5b056c
Namrita365 <22kt1a0576@gmail.com>
2025-10-17 11:04:25 +0530

    parameterized constructor

--- self_learn_java_from_yt/oops/parameterized_constructor.java
+++ self_learn_java_from_yt/oops/parameterized_constructor.java
@@ -13,10 +13,13 @@ class Student{
         this.gen=gen;
 
     }
+    public void info(){
+        System.out.println("Hello my i am a "+this.gen+" and my name is "+this.name+" and i am "+this.age+" years old!!");
+    }
 }
 public class parameterized_constructor {
         public static void main(String args[]){
             Student s1=new Student("Sandhya",19,'f');
-            s1.
+            s1.info();
         }
 }
