fa5bbe085f69eadc92903e49ef879c9476dc5dcc
Namrita365 <22kt1a0576@gmail.com>
2025-10-20 00:39:26 +0530

    added copy constructor

--- self_learn_java_from_yt/oops/copyConstructor.java
+++ self_learn_java_from_yt/oops/copyConstructor.java
@@ -10,12 +10,19 @@ class Student{
         this.age=age;
 
     }
+    Student(Student s){
+        this.name=s.name;
+        this.age=s.age;
+        System.out.println("copy constructor called");
+
+    }
 }
 public class copyConstructor {
     public static void main(String args[]){
         Student s1=new Student("Priya",20);
-        s1.name="Namrita";
-        s1.age=21;
         s1.printInfo();
+        Student s2=new Student(s1);
+        s2.printInfo();
+
     }
 }
