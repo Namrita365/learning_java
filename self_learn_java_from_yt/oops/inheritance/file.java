a7b5c00205d82e53a67182c78dafafec0db1f717
Namrita365 <22kt1a0576@gmail.com>
2025-10-24 23:59:56 +0530

    add file

--- self_learn_java_from_yt/oops/inheritance/file.java
+++ self_learn_java_from_yt/oops/inheritance/file.java
@@ -1,9 +1,21 @@
 class Dog{
-
+    public void bark(){
+        System.out.println("it barks");
+    }
 }
 class puppy  extends Dog{
+    public void sleep(){
+        System.out.println("puppy sleeping");
 
+    }
 }
 public class file{
-    
+    public static void main(String args[]){
+
+        puppy p1=new puppy();
+        p1.sleep();
+        p1.bark();
+        p1.sleep();
+    }
+
 }
